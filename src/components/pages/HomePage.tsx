08d8ecc215bb59c493cf9e941c06e8327cb6d885
Nikita Schastnyy <smynay@yandex.ru>
2021-08-08 16:19:25 +0300

    refactor: add layout, data integration

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import MainTemplate from 'components/templates/main-template/MainTemplate';
+import { observer } from 'mobx-react-lite';
+import { taskStore } from 'storages/task.storage';
 
-const HomePage = () => {
-  return <MainTemplate />;
-};
+const HomePage = observer(() => {
+  return <MainTemplate title="Home" taskArray={taskStore.tasks} />;
+});
 
 export default HomePage;
--- src/components/templates/main-template/MainTemplate.tsx
+++ src/components/templates/main-template/MainTemplate.tsx
@@ -1,7 +1,21 @@
+import DefaultLayout, {
+  DefaultLayoutNestedPropTypes,
+} from 'components/layouts/default-layout/DefaultLayout';
+import TaskBoard from 'components/organisms/task-board/TaskBoard';
+import { ITask } from 'models/task';
+
 import './MainTemplate.scss';
 
-const MainTemplate = () => {
-  return <div>Main</div>;
+type PropTypes = DefaultLayoutNestedPropTypes & {
+  taskArray: ITask[];
+};
+
+const MainTemplate = ({ title, taskArray }: PropTypes) => {
+  return (
+    <DefaultLayout title={title}>
+      <TaskBoard taskArray={taskArray} />
+    </DefaultLayout>
+  );
 };
 
 export default MainTemplate;
